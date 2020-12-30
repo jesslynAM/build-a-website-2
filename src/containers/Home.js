37b1a1881ee1de2a09ac14da6a0fbe137f0f9c5b
unknown <jesslynAM>
2020-12-30 21:59:53 +0700

    Fix things

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,10 +5,22 @@ import React, { useState, useEffect, useMemo } from "react";
 import Header from "../components/Header";
 import WeatherImage from "../components/WeatherImage";
 
+import City from "../components/City";
+
 const weatherKey = `TODO`; // Your API Key here
 
 function Home() {
   // TODO
+  return (
+    <div className="flex flex-col h-screen bg-purple-500 text-6xl">
+      
+      <City cityName={"Tokyo"} temp={"10"} color={"bg-yellow-500"}/>
+      
+      <City cityName={"Jakarta"} temp={"29"} color={"bg-red-500"}/>
+
+      <City cityName={"Bandung"} temp={"17"} color={"bg-blue-500"}/>
+    </div>
+  );
 }
 
 export default Home;
