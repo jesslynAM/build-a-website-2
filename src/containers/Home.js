fd59b38636502da644e67dccc243a265954392f3
unknown <jesslynAM>
2021-01-01 02:01:33 +0700

    rm env

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,24 +1,65 @@
 import React, { useState, useEffect, useMemo } from "react";
-// import axios from "axios";
-// import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 import Header from "../components/Header";
 import WeatherImage from "../components/WeatherImage";
 
 import City from "../components/City";
 
-const weatherKey = `TODO`; // Your API Key here
-
 function Home() {
-  // TODO
-  return (
-    <div className="flex flex-col h-screen bg-purple-500 text-6xl">
-      
-      <City cityName={"Tokyo"} temp={"10"} color={"bg-yellow-500"}/>
-      
-      <City cityName={"Jakarta"} temp={"29"} color={"bg-red-500"}/>
+  const history = useHistory();
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("Jakarta");
 
-      <City cityName={"Bandung"} temp={"17"} color={"bg-blue-500"}/>
+  const [cities, setCities] = useState([
+    {
+      name: "Jakarta",
+      currentTemp: "0",
+    },
+  ]);
+
+  useEffect(() => {
+    // console.log(process.env.REACT_APP_WEATHER_KEY);
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`
+      )
+      .then(function (response) {
+        // Successful request
+        const weather = response.data;
+        setWeatherData(weather);
+      })
+      .catch(function (error) {
+        // The best practice of coding is to not use console.log
+        console.log(error);
+      });
+  }, [city]);
+
+  useEffect(() => {
+    const searchParams = history.location.search;
+    const urlParams = new URLSearchParams(searchParams);
+    const city = urlParams.get("city");
+    if (city) {
+      setCity(city);
+    }
+  }, [history]);
+
+  const { currentTemp } = useMemo(() => {
+    let currentTemp = "";
+    if (weatherData) {
+      currentTemp = `${Math.round(weatherData.main.temp)}°C`;
+    }
+    return {
+      currentTemp,
+    };
+  }, [weatherData]);
+
+  return (
+    <div className="flex flex-col h-screen bg-purple-300 text-6xl">
+      <City cityName={city} temp={currentTemp} color={"bg-yellow-200"} />
+      <City cityName={city} temp={currentTemp} color={"bg-blue-200"} />
+      <City cityName={city} temp={currentTemp} color={"bg-pink-200"} />
     </div>
   );
 }
